f57a1b7eb485fd12663f172e7037241112419c2e
Jaehong Kang <j.kang@ridi.com>
2020-04-24 14:11:06 +0900

    Revert "Try to fix memory circulation 2"
    This reverts commit f842a676ed311ce965a84542bb4b29db7327bc11.

--- Sources/TouchXML/CXHTMLDocument.m
+++ Sources/TouchXML/CXHTMLDocument.m
@@ -85,7 +85,7 @@
 
             _node = (xmlNodePtr)theDoc;
             NSAssert(_node->_private == NULL, @"TODO");
-            _node->_private = (__bridge void *)self; // Note. NOT retained (TODO think more about _private usage)
+            _node->_private = (__bridge_retained void *)self; // Note. retained (TODO think more about _private usage)
 
             if (xpathObject)
             xmlXPathFreeObject (xpathObject);
@@ -139,7 +139,7 @@
             if (theDoc != NULL)
                 {
                 _node = (xmlNodePtr)theDoc;
-                _node->_private = (__bridge void *)self; // Note. NOT retained (TODO think more about _private usage)
+                _node->_private = (__bridge_retained void *)self; // Note. retained (TODO think more about _private usage)
                 }
             else
                 {
